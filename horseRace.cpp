ba0b0fc124fa084d60e5dfb4428c1f32cc670b00
Emily <165333619+wh0lock@users.noreply.github.com>
2026-01-28 20:44:17 +0000

    attempt

--- horseRace.cpp
+++ horseRace.cpp
@@ -8,29 +8,43 @@ bool isWinner(int horseNum, int* horses);
 const int SIZE = 5;
 
 int main(){
-	int horses[] = {0, 1, 2, 3, 4, 5};
+	int horses[] = {0, 0, 0, 0, 0};
 	int horseNum = 0;
+        std::string enter;
+	bool keepGoing = true;
 
-	for (horseNum = 0; horseNum < SIZE; horseNum++){
-		advance(horseNum, horses);
-		std::cout << horseNum << std::endl;
-	} // end for
-	
-	return horseNum;
+	while (keepGoing){
+		for (horseNum = 0; horseNum < SIZE; horseNum++){
+			advance(horseNum, horses);
+			printLane(horseNum, horses);
+		} // end for
+		std::cout << "Press enter for another turn";
+		std::cin >> enter;
+		if (enter == ""){
+			keepGoing = true;
+		} else {
+			keepGoing = false;
+		} // end if
+	} // end while
 	return 0;
 } // end main
 
-void advance(horseNum){
+void advance(int horseNum, int* horses){
 	std::random_device rd;
 	std::uniform_int_distribution<int> dist(0, 1);
 
 	int coin = dist(rd);
 
-	if (coin == 1){
-		horseNum++;
-	} else {
-		horseNum == horseNum;
-	}
+	horses[horseNum] += coin;
 } // end advance
 
+void printLane(int horseNum, int* horses){
+	std::cout << horseNum << ".............." << std::endl;
+} // end printLane
 
+bool isWinner(int horseNum, int* horses){
+	if (horses[horseNum] = 5){
+		std::cout << horseNum << " WINS!!!" << std::endl;
+	} // end if
+	return 0;
+} // end isWinner
